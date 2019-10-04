bce56e0833378aa873519f06c1b8c18b18ce4178
Pouya <pouyaheydary@gmail.com>
2019-10-04 14:21:58 +0330

    end of session 2

--- app/src/main/java/com/sematec/sematecandroidbasicmehr98/MainActivity.java
+++ app/src/main/java/com/sematec/sematecandroidbasicmehr98/MainActivity.java
@@ -1,8 +1,9 @@
 package com.sematec.sematecandroidbasicmehr98;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +12,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.d("MYTAG", "in onCreate");
+    }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
 
+        Log.d("MYTAG", "in onStart");
+    }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("MYTAG", "in onStart");
 
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("MYTAG", "in onStart");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("MYTAG", "in onStart");
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("MYTAG", "in onStart");
 
     }
 }
